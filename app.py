cce872a631f9ad87cf1be3f7979479bec1e1b25e
kavyamistry <41460941+kavyamistry@users.noreply.github.com>
2021-04-17 16:07:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    prediction = model.predict(int_features)
 
     output = round(prediction[0], 2)
 
